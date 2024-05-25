eb88571a103939492aa1e3c09e8493cca95a97c8
kalyan10001 <venkatakalyan1000@gmail.com>
2024-05-25 14:52:39 +0530

    packagejson updated

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,16 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectMongoDB from './db/connectMongoDB.js';
 import session from 'express-session';
+import path from 'path';
+
 
 
 const app=express();
+
+const PORT=process.env.PORT || 5000;
+const __dirname=path.resolve();
+
+console.log(__dirname)
 
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
@@ -24,9 +31,6 @@ dotenv.config();
 
 app.use(cors());
 
-app.get("/",(req,res)=>{
-    res.send("server is ready");
-})
 
 
 app.use("/api/auth",authRoutes);
@@ -34,8 +38,12 @@ app.use("/api/users",userRoutes);
 app.use("/api/explore",exploreRoutes);
 
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 app.listen(5000,()=>{
-    console.log("server running on 5000");
+    console.log(`server running on port ${PORT}`);
     connectMongoDB();
 })
